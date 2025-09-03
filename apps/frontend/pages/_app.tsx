10de35b102e87531444654a0106ca9de5af5e1ce
161sam <sschimmelpfennig@proton.me>
2025-09-03 17:06:53 +0200

    Fix CSS bugs

---
+++ apps/frontend/pages/_app.tsx
@@ -0,0 +1,11 @@
+import type { AppProps } from 'next/app'
+import '../src/styles/globals.css'
+import { ThemeProvider } from '../src/lib/theme-provider'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
