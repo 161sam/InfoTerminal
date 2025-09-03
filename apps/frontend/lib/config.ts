97b7f8ffad239c92cb595b2be440a3ee1896a9a0
saschi161 <saschi161@gmail.com>
2025-09-03 10:53:29 +0200

    Create config.ts

---
+++ apps/frontend/lib/config.ts
@@ -0,0 +1,13 @@
+export const SEARCH_API = process.env.NEXT_PUBLIC_SEARCH_API || '';
+if (!SEARCH_API) console.warn('NEXT_PUBLIC_SEARCH_API is not set');
+
+export const GRAPH_API = process.env.NEXT_PUBLIC_GRAPH_API || '';
+if (!GRAPH_API) console.warn('NEXT_PUBLIC_GRAPH_API is not set');
+
+export const DOCENTITIES_API = process.env.NEXT_PUBLIC_DOCENTITIES_API || '';
+if (!DOCENTITIES_API) console.warn('NEXT_PUBLIC_DOCENTITIES_API is not set');
+
+export const NLP_API = process.env.NEXT_PUBLIC_NLP_API || '';
+if (!NLP_API) console.warn('NEXT_PUBLIC_NLP_API is not set');
+
+export const GRAFANA_URL = process.env.NEXT_PUBLIC_GRAFANA_URL || '';
